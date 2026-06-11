0f9b3ea0f8e39dcbbb02b89df4702bbcc4169c0a
gs_dat <dat.greenspeed@gmail.com>
2026-06-12 02:10:00 +0700

    fix: swagger

--- public/api/swagger-ui-init.js
+++ public/api/swagger-ui-init.js
@@ -1,7 +1,12 @@
-<html>
-<head><title>404 Not Found</title></head>
-<body>
-<center><h1>404 Not Found</h1></center>
-<hr></hr><center>nginx</center>
-</body>
-</html>
+/* swagger-ui-init.js: Initialize Swagger UI */
+window.onload = function () {
+  const ui = SwaggerUIBundle({
+    url: "/api/swagger.json",   // Adjust the URL to point to your OpenAPI definition
+    dom_id: "#swagger-ui",
+    deepLinking: true,
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: "StandaloneLayout"
+  });
+  window.ui = ui;
+};
