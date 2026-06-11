d9e720730d29363c9e04756c8818e8491915a3fd
gs_dat <dat.greenspeed@gmail.com>
2026-06-12 03:20:02 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,43 @@
-import { createApp } from './app.bootstrap';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
-  const app = await createApp();
+  const app = await NestFactory.create(AppModule);
 
-  await app.listen(process.env.PORT || 3000);
+  app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  app.setGlobalPrefix('api', {
+    exclude: ['/'],
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('QR INVITE API')
+    .setDescription('API documentation for the NestJS application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
