8df719bf9d154a628f0fb94cd90cf491a8e16bd0
gs_dat <dat.greenspeed@gmail.com>
2026-06-12 02:59:58 +0700

    fix vercel

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,30 @@
-import '../src/main';
+// import '../src/main';
+// api/index.ts
+
+import serverlessExpress from '@codegenie/serverless-express';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+
+let cachedServer: any;
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix('api');
+
+    await app.init();
+
+    const expressApp = app.getHttpAdapter().getInstance();
+
+    return serverlessExpress({
+        app: expressApp,
+    });
+}
+
+export default async function handler(req: any, res: any) {
+    if (!cachedServer) {
+        cachedServer = await bootstrap();
+    }
+
+    return cachedServer(req, res);
+}
